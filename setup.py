3c6d5ffc8b8f8e851aa29daa04192e4d2dffa854
Lucy Lu Wang <lucyw@lucyw.local>
2017-12-15 14:18:23 -0800

    setup files

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import setuptools
+
+setuptools.setup(
+    name='pathhier',
+    version='0.1',
+    url='http://github.com/lucylw/pathhier',
+    packages=setuptools.find_packages(),
+    install_requires=[
+    ],
+    tests_require=[
+    ],
+    zip_safe=False,
+    test_suite='py.test',
+    entry_points='',
+)
