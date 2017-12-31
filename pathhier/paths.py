16755451606508805ae57c73480d9a5c1b5a29a1
Lucy Lu Wang <lucee25@gmail.com>
2017-12-31 14:41:32 -0500

    added paths to pathways

--- pathhier/paths.py
+++ pathhier/paths.py
@@ -15,12 +15,48 @@ class PathhierPaths:
     gedevo_folder = "gedevo"
     gedevo_exec = "gedevo"
 
+    humancyc_dir = "humancyc"
+    kegg_dir = "kegg"
+    panther_dir = "panther"
+    pid_dir = "pid"
+    reactome_dir = "reactome"
+    smpdb_dir = "smpdb"
+    wikipath_dir = "wikipathways"
+
     def __init__(self, base_dir=DEFAULT_BASE_DIR):
         self.base_dir = base_dir
 
     @property
     def raw_data_dir(self):
         return os.path.join(self.base_dir, self.data_folder, self.raw_data_folder)
+
+    @property
+    def humancyc_raw_data_dir(self):
+        return os.path.join(self.raw_data_dir, self.humancyc_dir)
+
+    @property
+    def kegg_raw_data_dir(self):
+        return os.path.join(self.raw_data_dir, self.kegg_dir)
+
+    @property
+    def panther_raw_data_dir(self):
+        return os.path.join(self.raw_data_dir, self.panther_dir)
+
+    @property
+    def pid_raw_data_dir(self):
+        return os.path.join(self.raw_data_dir, self.pid_dir)
+
+    @property
+    def reactome_raw_data_dir(self):
+        return os.path.join(self.raw_data_dir, self.reactome_dir)
+
+    @property
+    def smpdb_raw_data_dir(self):
+        return os.path.join(self.raw_data_dir, self.smpdb_dir)
+
+    @property
+    def wikipathways_raw_data_dir(self):
+        return os.path.join(self.raw_data_dir, self.wikipath_dir)
 
     @property
     def processed_data_dir(self):
