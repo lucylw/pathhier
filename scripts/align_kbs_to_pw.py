0af7061b8220621633032d5ba53b2b2d77abc40d
Lucy Lu Wang <lucee25@gmail.com>
2018-03-06 22:18:37 -0800

    refactored KBAligner class; also, added different features for generating similarity scores

--- scripts/align_kbs_to_pw.py
+++ scripts/align_kbs_to_pw.py
@@ -1,6 +1,7 @@
 #!python
 
 import os
+import sys
 import json
 import numpy as np
 import itertools
@@ -12,137 +13,189 @@ import pathhier.constants as constants
 from pathhier.candidate_selector import CandidateSelector
 
 
-def get_idf_dict(kb, i_to_w):
-    """
-    Construct idf mapping dict between word ind and idf score in kb
-    :param kb:
-    :param i_to_w:
-    :return:
-    """
-    idf_dict = defaultdict(float)
-    n = len(kb)
+# class for aligning a pathway KB to the Pathway Ontology
+class KBAligner:
+    def __init__(self, kb_name):
+        """
+        Initialize and load all files
+        :param kb_name:
+        """
+        self.kb_name = kb_name
 
-    for ind in i_to_w:
-        names_with_word = [k for k, v in kb.items() if ind in v['name_tokens']]
-        if names_with_word:
-            idf = np.log(n / (len(names_with_word) + 1))
-        else:
-            idf = np.log(n / 1)
-        idf_dict[ind] = idf
+        paths = PathhierPaths()
+        kb_file = os.path.join(paths.output_dir, kb_name + '_ontology.json')
+        pw_file = os.path.join(paths.output_dir, 'pw.json')
 
-    return idf_dict
+        assert os.path.exists(kb_file)
+        assert os.path.exists(pw_file)
 
+        # load KB to align to PW
+        with open(kb_file, 'r') as f:
+            self.kb = json.load(f)
 
-def compute_weighted_token_jaccard(
-        s_toks: List,
-        t_toks: List,
-        s_idfs: Dict,
-        t_idfs: Dict
-):
-    """
-    Compute weighted token jaccard index between the set of tokens from source entity and target entity
-    :param s_toks: tokens from source entity
-    :param t_toks: tokens from target entity
-    :param s_idfs: idf scores of tokens from source kb
-    :param t_idfs: idf scores of tokens from target kb
-    :return:
-    """
-    tok_intersect = set(s_toks).intersection(set(t_toks))
-    tok_union = set(s_toks).union(set(t_toks))
+        # load pathway ontology
+        with open(pw_file, 'r') as f:
+            self.pw = json.load(f)
 
-    weighted_numerator = 0.
-    for itok in tok_intersect:
-        weighted_numerator += 0.5*(s_idfs[itok] + t_idfs[itok])
+        self.cand_sel = CandidateSelector(self.kb, self.pw)
+        self.score_list = []
+        self.inst_list = []
 
-    weighted_denominator = 0.
-    for utok in tok_union:
-        weighted_denominator += 0.5*(s_idfs[utok] + t_idfs[utok])
+        self.output_file = os.path.join(paths.output_dir, '{}_pw_alignment.tsv'.format(kb_name))
+        self.instance_file = os.path.join(paths.output_dir, '{}_pw_instance_alignment.tsv'.format(kb_name))
 
-    return weighted_numerator / weighted_denominator
+    def compute_weighted_jaccard(
+            self,
+            s_toks: List,
+            t_toks: List
+    ):
+        """
+        Compute weighted token jaccard index between the set of tokens from source entity and target entity
+        :param s_toks: tokens from source entity
+        :param t_toks: tokens from target entity
+        :return:
+        """
+        tok_intersect = set(s_toks).intersection(set(t_toks))
+        tok_union = set(s_toks).union(set(t_toks))
 
+        weighted_numerator = 0.
+        for itok in tok_intersect:
+            weighted_numerator += 0.5*(self.cand_sel.s_token_to_idf[itok] + self.cand_sel.t_token_to_idf[itok])
 
-def compute_unweighted_jaccard(
-        s_ngms: List,
-        t_ngms: List
-):
-    """
-    Compute jaccard index between the set of ngrams from source entity and target entity
-    :param s_ngms: ngrams from source entity
-    :param t_ngms: ngrams from target entity
-    :return:
-    """
-    ngm_intersect = set(s_ngms).intersection(set(t_ngms))
-    ngm_union = set(s_ngms).union(set(t_ngms))
-    return len(ngm_intersect) / len(ngm_union)
+        weighted_denominator = sys.float_info.epsilon
+        for utok in tok_union:
+            weighted_denominator += 0.5*(self.cand_sel.s_token_to_idf[utok] + self.cand_sel.t_token_to_idf[utok])
 
+        return weighted_numerator / weighted_denominator
 
-paths = PathhierPaths()
+    @staticmethod
+    def compute_unweighted_jaccard(
+            s_ngms: List,
+            t_ngms: List
+    ):
+        """
+        Compute jaccard index between the set of ngrams from source entity and target entity
+        :param s_ngms: ngrams from source entity
+        :param t_ngms: ngrams from target entity
+        :return:
+        """
+        ngm_intersect = set(s_ngms).intersection(set(t_ngms))
+        ngm_union = set(s_ngms).union(set(t_ngms))
+        return len(ngm_intersect) / len(ngm_union)
 
-source_names = ['biocyc', 'reactome']
+    def align(self):
+        """
+        Align kb with PW
+        :return:
+        """
+        done_pairs = set([])
+        self.score_list = []
+        self.inst_list = []
 
-for source_kb_name in source_names:
-    source_json = source_kb_name + '_ontology.json'
+        for p_id, p_info in self.kb.items():
 
-    source_file = os.path.join(paths.output_dir, source_json)
-    target_file = os.path.join(paths.output_dir, 'pw.json')
+            # list of matches in PW
+            matches = []
 
-    with open(source_file, 'r') as f:
-        s_ont = json.load(f)
+            for pw_class in self.cand_sel.select(p_id)[:constants.KEEP_TOP_N_CANDIDATES]:
+                scores = dict()
 
-    with open(target_file, 'r') as f:
-        t_ont = json.load(f)
+                scores['name_token_jaccard'] = self.compute_weighted_jaccard(
+                    p_info['name_tokens'],
+                    self.pw[pw_class]['name_tokens']
+                )
 
-    selector = CandidateSelector(s_ont, t_ont)
-    jacc_list = []
-    done_pairs = set([])
-    instance_matches = []
+                scores['name_ngram_jaccard'] = self.compute_unweighted_jaccard(
+                    p_info['name_ngrams'],
+                    self.pw[pw_class]['name_ngrams']
+                )
 
-    for s_pw, val in s_ont.items():
-        insts = s_ont[s_pw]['instances']
-        t_matches = []
-        for t_pw in selector.select(s_pw)[:constants.KEEP_TOP_N_CANDIDATES]:
-            s_tokens = val['name_tokens']
-            t_tokens = t_ont[t_pw]['name_tokens']
-            jacc = compute_weighted_token_jaccard(s_tokens, t_tokens, selector.s_token_to_idf, selector.t_token_to_idf)
+                scores['alias_token_jaccard'] = self.compute_unweighted_jaccard(
+                    p_info['alias_tokens'],
+                    self.pw[pw_class]['alias_tokens']
+                )
 
-            s_ngrams = val['name_ngrams']
-            t_ngrams = t_ont[t_pw]['name_ngrams']
-            ngram_jacc = compute_unweighted_jaccard(s_ngrams, t_ngrams)
+                scores['def_token_jaccard'] = self.compute_weighted_jaccard(
+                    p_info['def_tokens'],
+                    self.pw[pw_class]['def_tokens']
+                )
 
-            t_matches.append((t_pw, jacc, ngram_jacc))
+                scores['all_token_jaccard'] = self.compute_weighted_jaccard(
+                    p_info['all_tokens'],
+                    self.pw[pw_class]['all_tokens']
+                )
 
-        if t_matches:
-            t_matches.sort(key=lambda x: x[1] + x[2], reverse=True)
-            best_t, best_jacc = t_matches[0]
-            if (s_pw, best_t) not in done_pairs:
-                jacc_list.append((s_pw, best_t, best_jacc))
-                for i in insts:
-                    instance_matches.append((i, best_t, best_jacc))
-                done_pairs.add((s_pw, best_t))
+                max_score = max(scores.values())
+                mean_score = np.mean(list(scores.values()))
 
-    instance_matches.sort(key=lambda x: x[2], reverse=True)
-    jacc_list.sort(key=lambda x: x[2], reverse=True)
+                matches.append((pw_class, max_score, mean_score, scores))
 
-    output_file = os.path.join(paths.output_dir, '{}_pw_alignment.tsv'.format(source_kb_name))
-    instance_file = os.path.join(paths.output_dir, '{}_pw_instance_alignment.tsv'.format(source_kb_name))
+            if matches:
+                # sort matches by best similarity score
+                matches.sort(key=lambda x: x[1], reverse=True)
 
-    with open(output_file, 'w') as outf:
-        outf.write('score\t{}_id\t{}_name\tpw_id\tpw_name\n'.format(
-            source_kb_name, source_kb_name
-        ))
-        for s_id, t_id, score in jacc_list:
-            outf.write('%f\t%s\t%s\t%s\t%s\n' % (
-                score, s_id, s_ont[s_id]['name'], t_id, t_ont[t_id]['name']
+                # best match class
+                best_pw, best_simscore, mean_simscore = matches[0][:3]
+                self.score_list.append(('best', p_id, best_pw, best_simscore, mean_simscore))
+                for inst in p_info['instances']:
+                    self.inst_list.append(('best', inst, best_pw, best_simscore, mean_simscore))
+
+                for m in matches[1:]:
+                    best_pw, best_simscore, mean_simscore = m[:3]
+                    if best_simscore >= constants.SIMSCORE_THRESHOLD or mean_simscore >= constants.SIMSCORE_THRESHOLD \
+                            and (p_id, best_pw) not in done_pairs:
+                        self.score_list.append(('other', p_id, best_pw, best_simscore, mean_simscore))
+                        for inst in p_info['instances']:
+                            self.inst_list.append(('other', inst, best_pw, best_simscore, mean_simscore))
+                        done_pairs.add((p_id, best_pw))
+
+        self.inst_list.sort(key=lambda x: x[3], reverse=True)
+        self.score_list.sort(key=lambda x: (x[1], 1-x[3]))
+
+        self.write_to_file()
+
+        return
+
+    def write_to_file(self):
+        """
+        Write score_list to output_file and inst_list to instance_file
+        :return:
+        """
+        with open(self.output_file, 'w') as outf:
+            outf.write('match_type\tmax_score\tmean_score\t{}_id\t{}_name\tpw_id\tpw_name\n'.format(
+                self.kb_name, self.kb_name
             ))
+            for match_type, kb_id, pw_id, max_score, mean_score in self.score_list:
 
-    with open(instance_file, 'w') as outf:
-        outf.write('score\t{}_instance_id\tpw_id\tpw_name\n'.format(
-            source_kb_name
-        ))
-        for s_inst, t_id, score in instance_matches:
-            outf.write('%f\t%s\t%s\t%s\n' % (
-                score, s_inst, t_id, t_ont[t_id]['name']
+                # adjust pathway ID for BioCyc pathways
+                kb_id_use = kb_id
+                if self.kb_name == 'biocyc':
+                    kb_id_use = 'BioCyc:' + kb_id
+
+                outf.write('%s\t%.2f\t%.2f\t%s\t%s\t%s\t%s\n' % (
+                    match_type, max_score, mean_score, kb_id_use,
+                    self.kb[kb_id]['name'], pw_id, self.pw[pw_id]['name']
+                ))
+
+        with open(self.instance_file, 'w') as outf:
+            outf.write('match_type\tmax_score\tmean_score\t{}_instance_id\tpw_id\tpw_name\n'.format(
+                self.kb_name
             ))
+            for match_type, kb_inst, pw_id, max_score, mean_score in self.inst_list:
+                outf.write('%s\t%.2f\t%.2f\t%s\t%s\t%s\n' % (
+                    match_type, max_score, mean_score, kb_inst, pw_id, self.pw[pw_id]['name']
+                ))
+
+
+# list of KBs to align
+kb_names = ['biocyc', 'reactome']
+
+# align each KB in list
+for name in kb_names:
+    aligner = KBAligner(name)
+    aligner.align()
+
+
 
 
 
