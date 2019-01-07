e71e467028a1f18662b1b7e439c068e7481dd416
Lucy Lu Wang <lucee25@gmail.com>
2019-01-06 16:35:31 -0800

    open fasttext vectors with tar

--- pathhier/pathway_aligner.py
+++ pathhier/pathway_aligner.py
@@ -4,6 +4,7 @@ import csv
 import gzip
 import tqdm
 import json
+import tarfile
 import string
 import random
 import pickle
@@ -73,9 +74,9 @@ class PathAligner:
         self.chebi_lookup = pickle.load(open(os.path.join(paths.processed_data_dir, 'chebi_lookup.pickle'), 'rb'))
         self.uniprot_lookup = pickle.load(open(os.path.join(paths.processed_data_dir, 'uniprot_lookup.pickle'), 'rb'))
 
-        # # create bioservices services
-        # self.chebi_db = ChEBI()
-        # self.uniprot_db = UniProt()
+        # create bioservices services
+        self.chebi_db = ChEBI()
+        self.uniprot_db = UniProt()
 
         # tokenizers and stop words
         self.tokenizer = RegexpTokenizer(r'[A-Za-z\d]+')
@@ -96,12 +97,12 @@ class PathAligner:
         print("Loading fasttext vectors...")
         self.fasttext = dict()
         if ft_file and os.path.exists(ft_file):
-            with gzip.open(ft_file, 'rb') as f:
-                content = f.read().split(b'\n')
+            with tarfile.open(ft_file, 'r:gz') as f:
+                content = f.read().split('\n')
             for l in content:
                 vec = l.split()
                 if vec and len(vec) > 1:
-                    self.fasttext[vec[0].decode('utf-8')] = [float(val) for val in vec[1:]]
+                    self.fasttext[vec[0]] = [float(val) for val in vec[1:]]
 
         # struc2vec path
         self.s2v_path = s2v_path
